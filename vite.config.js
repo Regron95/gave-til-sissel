b7d0c58b5638d6fb572390f26ef1cbba61334557
Regron95 <rronesen@outlook.com>
2025-11-15 19:10:15 +0100

    Löste merge-konflikt i vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '/gave-til-sissel/',
-=======
->>>>>>> d61edf806c02ef9b3b9ca5f74051ffca88601c44
   plugins: [react()],
 })
